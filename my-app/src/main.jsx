608fa1d9296d2b6aa126ba6a48c13176c9b5c0a2
VanAnhPH13707 <79831956+VanAnhPH13707@users.noreply.github.com>
2022-03-09 15:47:34 +0700

    update

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+const myName = "Nguyễn Vân Anh";
+const myAge = 20;
+const myStatus = true;
+const person = {
+  name: "Vân Anh",
+  age: 20
+}
+const showInfo = (props) =>{
+  console.log(props);
+  return <p>Thông tin user {props.name}</p>
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
+  
   document.getElementById('root')
 )
