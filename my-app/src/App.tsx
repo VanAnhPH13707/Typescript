5cc7077150b96aae2b480859621157d6cc8e2500
VanAnhPH13707 <79831956+VanAnhPH13707@users.noreply.github.com>
2022-03-16 10:44:38 +0700

    update

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,20 +1,55 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import logo from './logo.svg'
 import './App.css'
-import ShowInfo from './ShowInfo'
+import ShowInfo from './components/ShowInfo'
+import type { ProductType } from './types/product';
+import { list, remove } from './api/product';
 
-type TProduct = {
-  id: number,
-  name: string
-}
+
 
 function App() {
-  const [count, setCount] = useState<number>(0);
-  const [products, setProducts] = useState<TProduct[]>([{id: 1, name: "Dat"}])
 
+  const [products, setProducts] = useState<ProductType[]>([]);
+  // const [count, setCount] = useState<number>(0);
+
+  useEffect(() => {
+    const getProducts = async () =>{
+      const {data} = await list();
+      setProducts(data);
+    }
+    getProducts();
+
+  }, [])
+  
+  const removeItem = async (id: number) => {
+    // xoa tren API
+    const { data } = await remove(id);
+    // reRender
+    data && setProducts(products.filter(item => item.id !== data.id));
+  }
   return (
     <div className="App">
-        <ShowInfo name="Dat"/>
+        <hr />
+        <table>
+        <thead>
+          <th>#</th>
+          <th>Name</th>
+          <th></th>
+        </thead>
+        <tbody>
+          {products.map((item, index) => {
+            return <tr>
+                    <td>{index + 1}</td>
+                    <td>{item.name}</td>
+                    <td>
+                      <button onClick={() => removeItem(item.id)}>Remove</button>
+                    </td>
+                  </tr>
+          })}
+          
+        </tbody>
+      </table>
     </div>
   )
 }
